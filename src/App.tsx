cc9bf4bce09e33dc86c83a91d4062659721b0e06
harshil <harshil4076@gmail.com>
2023-01-13 10:39:44 -0500

    Added calculate formula and styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,58 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [monthlyPayment,setMonthlyPayment] = useState<number>()
+  const [monthlyPayment,setMonthlyPayment] = useState<number | null>(null)
+  const [principalAmount, setPrincipalAmount] = useState<number |null>(null)
+  const [interestRate, setInterestRate] = useState<number | null>(null)
+  const [term, setTerm] = useState<number | null>(null)
+
+  function handleCalculate(){
+    if(principalAmount !== null && interestRate !== null && term!==null){
+      let paymentCount = term * 12
+      console.log("paymentcount", paymentCount)
+      let actualInterest = (interestRate/100)/12
+      console.log("actual interest", actualInterest)
+      let calculate = principalAmount * (
+        (actualInterest * ((1 + actualInterest)**paymentCount))/
+        (((1+actualInterest)**paymentCount)-1)
+        )
+      setMonthlyPayment(calculate)
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
         Mortgage Calculator
       </header>
       <div className='form-container'>
-        <label>Enter Principal amount:
-        <input className="principal-input" name="principal-input" type="number" />
+        <label className='input-labels'>Enter Principal amount $
+        <input 
+          className="principal-input" 
+          onChange={(event) => setPrincipalAmount(parseInt(event.target.value))} 
+          name="principal-input" 
+          type="number" />
         </label>
-        <label>Enter interest rate:
-          <input className="interest-input" name="interest-input" />
+        <label className='input-labels'>Enter interest rate %
+          <input 
+            className="interest-input" 
+            onChange={event => setInterestRate(parseInt(event.target.value))} 
+            name="interest-input" 
+            type="number"/>
+            
         </label>
-        <label>Length of loan:
-          <input className='loan-input' name="loan-input"/>
+        <label className='input-labels'>Length of loan in Years
+          <input 
+            className='loan-input' 
+            onChange={event => setTerm(parseInt(event.target.value))} 
+            name="loan-input" 
+            type="number"/>
+            
         </label>
         <div>
-        <input type="button" value="Calculate" />
+        <input 
+          type="button" 
+          onClick={handleCalculate} 
+          value="Calculate" />
         </div>
         <div>
           <p>Your monthly mortgage payment will be <span>{monthlyPayment}</span></p>
